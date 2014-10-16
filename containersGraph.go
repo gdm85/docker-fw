d1545bdf46accdd37e15d4d01e4dd84d84446d40
gdm85 <gdm85@users.noreply.github.com>
2014-10-16 15:05:54 +0200

    Fix sort order of containers to start

--- containersGraph.go
+++ containersGraph.go
@@ -1,6 +1,6 @@
 /*
  * docker-fw v0.1.0 - a complementary tool for Docker to manage custom
- * 					  firewall rules between/towards Docker containers
+ *                    firewall rules between/towards Docker containers
  * Copyright (C) 2014 gdm85 - https://github.com/gdm85/docker-fw/
 
 This program is free software; you can redistribute it and/or
@@ -31,8 +31,8 @@ import (
 type Node struct {
 	Self *docker.Container
 	// all nodes that hierarchically come afterwards
-	ParentOf SortableNodeArray
-	Started  bool
+	Leaves  SortableNodeArray
+	Visited bool
 }
 
 type SortableNodeArray []*Node
@@ -45,19 +45,12 @@ func (s SortableNodeArray) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
 }
 
-// inverted sort order, to sort for biggest to smallest
 func (s SortableNodeArray) Less(i, j int) bool {
-	return !s.realLess(i, j)
-}
-
-func (s SortableNodeArray) realLess(i, j int) bool {
-	// first check if there is a parent/child relationship
-	// children leave room for parents
-	// a container that is used by another is a parent
-	if s[i].ParentOf.Contains(s[j]) {
+	// first check if there is a parent/leaf relationship
+	if s[i].Leaves.Contains(s[j]) {
 		return false
 	}
-	if s[j].ParentOf.Contains(s[i]) {
+	if s[j].Leaves.Contains(s[i]) {
 		return true
 	}
 
@@ -65,7 +58,7 @@ func (s SortableNodeArray) realLess(i, j int) bool {
 
 	// when no relationship is estabilished, then just sort by number of other relationships
 	// will be undetermined in case of 0
-	return len(s[i].ParentOf) < len(s[j].ParentOf)
+	return len(s[i].Leaves) < len(s[j].Leaves)
 }
 
 func (s SortableNodeArray) Contains(n *Node) bool {
@@ -87,14 +80,109 @@ func arrayContains(haystack []*docker.Container, needle *docker.Container) bool 
 	return false
 }
 
-func recurseStart(nodes []*Node, paused bool) error {
-	// proceed to start all nodes
+func sortBeforeStart(result *Node, nodes []*Node) (*Node, error) {
 	for _, node := range nodes {
 		// skip already-started nodes, possible when a node is used by multiple nodes
-		if node.Started {
+		if node.Visited {
 			continue
 		}
+		node.Visited = true
+		result.Leaves = append(result.Leaves, node)
 
+		// recurse dependencies
+		_, err := sortBeforeStart(result, node.Leaves)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return result, nil
+}
+
+// 1) build a graph of container dependencies
+// 2) start them from lowest to highest dependency count
+// 3) for each container start, pause them (if asked to)
+// 4) for each container start, run the 'replay' action too
+func StartContainers(containerIds []string, paused, pullDeps bool) error {
+	// first normalize all container ids to the proper 'ID' property given through inspect
+	// this is necessary because we won't allow to start dependant containers if not specified
+	var containers []*docker.Container
+	for _, cid := range containerIds {
+		container, err := ccl.LookupInert(cid)
+		if err != nil {
+			return err
+		}
+
+		containers = append(containers, container)
+	}
+
+	// traverse the containers list identifying leaf|parent relationships
+	graph := make(map[string]*Node, 0)
+	for _, container := range containers {
+
+		// prepare container node itself
+		node, ok := graph[container.ID]
+		if !ok {
+			node = &Node{
+				Self: container,
+			}
+			graph[container.ID] = node
+		}
+
+		for _, link := range container.HostConfig.Links {
+			parts := strings.SplitN(link, ":", 2)
+
+			// identify the target container
+			linkTarget := parts[0][1:]
+			targetContainer, err := ccl.LookupInert(linkTarget)
+			if err != nil {
+				return err
+			}
+
+			// allow to pull in other containers only if specifically allowed to
+			if !pullDeps {
+				if !arrayContains(containers, targetContainer) {
+					return errors.New(fmt.Sprintf("container '%s' is not specified in list and no --pull-deps specified", targetContainer.Name[1:]))
+				}
+			}
+
+			targetNode, ok := graph[targetContainer.ID]
+			if !ok {
+				targetNode = &Node{
+					Self: targetContainer,
+				}
+
+				graph[targetContainer.ID] = targetNode
+			}
+
+			// now create association
+			targetNode.Leaves = append(targetNode.Leaves, node)
+		}
+	}
+
+	// convert the map to a flat array
+	var nodes SortableNodeArray
+	for _, v := range graph {
+		nodes = append(nodes, v)
+	}
+
+	// sort by dependencies/links number
+	// order is: from least used to most used
+	sort.Sort(nodes)
+
+	var result Node
+	_, err := sortBeforeStart(&result, nodes)
+	if err != nil {
+		return err
+	}
+
+	dryRun := false
+
+	for i:=len(result.Leaves)-1;i>=0;i-- {
+		node := result.Leaves[i]
+		if dryRun {
+			fmt.Printf("%s\n", node.Self.Name[1:])
+			continue
+		}
 		changedState := false
 		// start container
 		if !node.Self.State.Running {
@@ -128,92 +216,7 @@ func recurseStart(nodes []*Node, paused bool) error {
 		if err != nil {
 			return err
 		}
-
-		// mark as started and disable further access
-		node.Self = nil
-		node.Started = true
-
-		// recurse into children
-		err = recurseStart(node.ParentOf, paused)
-		if err != nil {
-			return err
-		}
 	}
-	return nil
-}
-
-// 1) build a graph of container dependencies
-// 2) start them from lowest to highest dependency count
-// 3) for each container start, pause them (if asked to)
-// 4) for each container start, run the 'replay' action too
-func StartContainers(containerIds []string, paused, pullDeps bool) error {
-	// first normalize all container ids to the proper 'ID' property given through inspect
-	// this is necessary because we won't allow to start dependant containers if not specified
-	var containers []*docker.Container
-	for _, cid := range containerIds {
-		container, err := ccl.LookupInert(cid)
-		if err != nil {
-			return err
-		}
-
-		containers = append(containers, container)
-	}
-
-	// traverse the containers list identifying children|parent relationships
-	graph := make(map[string]*Node, 0)
-	for _, container := range containers {
-
-		for _, link := range container.HostConfig.Links {
-			parts := strings.SplitN(link, ":", 2)
-
-			// prepare container node itself
-			node, ok := graph[container.ID]
-			if !ok {
-				node = &Node{
-					Self: container,
-				}
-
-				graph[container.ID] = node
-			}
-
-			// identify the target container
-			linkTarget := parts[0][1:]
-			targetContainer, err := ccl.LookupInert(linkTarget)
-			if err != nil {
-				return err
-			}
-
-			// allow to pull in other containers only if specifically allowed to
-			if !pullDeps {
-				if !arrayContains(containers, targetContainer) {
-					return errors.New(fmt.Sprintf("container '%s' is not specified in list and no --pull-deps specified", targetContainer.Name[1:]))
-				}
-			}
-
-			targetNode, ok := graph[linkTarget]
-			if !ok {
-				targetNode = &Node{
-					Self: targetContainer,
-				}
-
-				graph[linkTarget] = targetNode
-			}
-
-			// now create the reverse 1-way association
-			targetNode.ParentOf = append(targetNode.ParentOf, node)
-		}
-	}
-
-	// convert the map to a flat array
-	var nodes SortableNodeArray
-	for _, v := range graph {
-		nodes = append(nodes, v)
-	}
-
-	// sort by dependencies/links number
-	sort.Sort(nodes)
-
-	err := recurseStart(nodes, paused)
 
 	return err
 }
